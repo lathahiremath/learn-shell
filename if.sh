0dbecb47be8c94d912d863f23e2ac94ce2acba70
Latha <lathahiremath03@gmail.com>
2023-04-24 14:27:56 +0530

    if else

--- if.sh
+++ if.sh
@@ -3,8 +3,9 @@ quantity=$2
 
 if [ "$fruit_name"=="mango"]
   then
-    echo mango quantity=$quantity
+    echo mango quantity - $quantity
 
    else
      echo fruit does not exist
 fi
+ note: always double quote variables in expressions
